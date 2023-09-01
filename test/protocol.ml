3f85c81d5862bff562130e92e3adca72644995b9
Sven <fredrik-98@live.se>
2023-09-02 00:14:46 +0200

    test: promote the test

--- test/protocol.ml
+++ test/protocol.ml
@@ -14,8 +14,11 @@ let%expect_test "DNS Packet" =
   |> Cstruct.of_string
   |> Dns.Protocol.DnsHeader.read
   |> Dns.Protocol.DnsHeader.show
-  |> Eio.traceln "%S";
-  (* Eio.Stdenv.cwd *)
-  printf "%d" (1 + 2);
-  [%expect {| 3 |}]
+  |> print_endline;
+  [%expect {|
+    { Protocol.DnsHeader.id = 34346; query_response = true; opcode = 0;
+      authoritative_answer = false; truncated_message = false;
+      recursion_desired = true; recursion_available = true; z = 0;
+      rescode = Protocol.ResultCode.NOERROR; questions = 1; answers = 1;
+      authoritative_entries = 0; resource_entries = 0 } |}]
 ;;
